8e41ea597f3c53b6cb5fbc1f142b9ef751d59142
Braian Barrientos <braianbts@gmail.com>
2023-09-27 14:17:34 -0300

    corrections completed, check if MONGO_URI is done correctly

--- api/config/db.js
+++ api/config/db.js
@@ -1,15 +1,19 @@
 // Configuracion DB
+require("dotenv").config();
 const mongoose = require("mongoose");
 
-const dbURI = "mongodb://localhost:27017/firstplug";
-
-mongoose
-  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log(`Database is connected to ${dbURI}`);
-  })
-  .catch((error) => {
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`Database is connected to ${process.env.MONGO_URI}`);
+  } catch (error) {
     console.error("Database connection error:", error);
-  });
+  }
+};
+
+connectToDatabase();
 
 module.exports = mongoose;
--- server.js
+++ server.js
@@ -1,12 +1,32 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("./api/config/db");
 const app = express();
-const port = 3001;
+const cors = require("cors");
+const morgan = require("morgan");
+
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(
+  cors({
+    origin: process.env.FIRST_PLUG_PLATFORM_CLIENT_HOST,
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("ok");
 });
 
-app.listen(port, () => {
-  console.log(`Servidor corriendo en el puerto ${port}`);
+mongoose.connection.once("open", () => {
+  console.log("Base de datos conectada correctamente");
+});
+
+mongoose.connection.on("error", (error) => {
+  console.error("Error en la conexión a la base de datos:", error);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
 });
