f6dce7b466a65a50ceb03d64c05d6ae81bf455cc
Esteban Rodriguez <nahuelr.developer@gmail.com>
2023-10-03 16:59:22 -0300

    modify auth middleware

--- api/middleware/auth.middleware.js
+++ api/middleware/auth.middleware.js
@@ -1,22 +1,22 @@
 const { validateToken } = require("../config/token");
 
-const validateUser = (req, res, next) => {
-  const token = req.headers.authorization;
+class AuthMiddleware {
+  static validateUser(req, res, next) {
+    const token = req.headers.authorization;
 
-  if (!token) {
-    return res.status(401).json({ message: "Missing authentication token" });
+    if (!token) {
+      return res.status(401).json({ message: "Missing authentication token" });
+    }
+
+    const user = validateToken(token.replace("Bearer ", ""));
+
+    if (!user) {
+      return res.status(401).json({ message: "Missing user" });
+    }
+
+    req.user = user;
+    next();
   }
+}
 
-  const user = validateToken(token.replace("Bearer ", ""));
-
-  if (!user) {
-    return res.status(401).json({ message: "Missing user" });
-  }
-
-  req.user = user;
-  next();
-};
-
-module.exports = {
-  validateUser,
-};
+module.exports = AuthMiddleware;
--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const AuthControllers = require("../controllers/auth.controllers.js");
-const { validateUser } = require("../middleware/auth.middleware.js");
+const AuthMiddleware = require("../middleware/auth.middleware.js");
 
 router.post("/register", AuthControllers.register);
 router.post("/login", AuthControllers.login);
 router.post("/logout", AuthControllers.logout);
-router.get("/me", validateUser, AuthControllers.me);
+router.get("/me", AuthMiddleware.validateUser, AuthControllers.me);
 
 module.exports = router;
