3feb9b19f2653804c9e73a1c8bae5a7cb0f428ef
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-10-05 18:01:14 -0300

    adding services, controllers and routes of Order models

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const authRoutes = require("./auth.routes");
+const ordersRoutes = require("./orders.routes");
 
 router.use("/auth", authRoutes);
+router.use("/orders", ordersRoutes);
 
 module.exports = router;
