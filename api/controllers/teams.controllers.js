9edd0dc1a632a534257e9c21a31d45f8332f412d
Tute22 <agustinsandoval1601@gmail.com>
2023-10-06 10:21:46 -0300

    next and remove console.error in teams.controllers

--- api/controllers/teams.controllers.js
+++ api/controllers/teams.controllers.js
@@ -1,29 +1,27 @@
 const TeamsServices = require("../services/teams.services");
 
 class TeamsController {
-  static async getAll(req, res) {
+  static async getAll(req, res, next) {
     try {
       const teams = await TeamsServices.getAllTeams();
 
       res.json(teams);
     } catch (error) {
-      res.status(400).json({ "Error obtaining equipment": error });
-      console.error(error);
+      next(error);
     }
   }
 
-  static async newTeam(req, res) {
+  static async newTeam(req, res, next) {
     try {
       const team = await TeamsServices.createTeam(req.body);
 
       res.status(201).json(team);
     } catch (error) {
-      res.status(401).json({ "Error creating team": error });
-      console.error(error);
+      next(error);
     }
   }
 
-  static async updateTeam(req, res) {
+  static async updateTeam(req, res, next) {
     try {
       const { idTeam } = req.params;
 
@@ -31,19 +29,17 @@ class TeamsController {
 
       res.status(200).json(updatedTeam);
     } catch (error) {
-      res.status(400).json({ "Error updating team": error });
-      console.error(error);
+      next(error);
     }
   }
 
-  static async deleteTeam(req, res) {
+  static async deleteTeam(req, res, next) {
     try {
       const { idTeam } = req.params;
       const deletedTeam = await TeamsServices.deleteTeam(idTeam);
       res.status(200).json(deletedTeam);
     } catch (error) {
-      res.status(400).json({ "Error deleting team": error });
-      console.error(error);
+      next(error);
     }
   }
 }
