c616b57cee245865429b1713b9b1094f95e17c20
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-10-06 10:43:17 -0300

    adding productRoutes import on index routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const authRoutes = require("./auth.routes");
-
 const teamMembersRoutes = require("../routes/teamMembers.routes");
-
 const teamsRoutes = require("./teams.routes");
-
 const ordersRoutes = require("./orders.routes");
-
+const productRoutes = require("./product.routes");
 
 router.use("/auth", authRoutes);
 router.use("/teamMembers", teamMembersRoutes);
@@ -16,10 +13,6 @@ router.use("/teams", teamsRoutes);
 
 router.use("/orders", ordersRoutes);
 
-
-
 router.use("/products", productRoutes);
-
-
 
 module.exports = router;
