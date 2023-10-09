1360dedfcf2fb9a0ef4e3a6d391fcaa931cb9de6
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2023-10-09 11:34:45 -0300

    fixed status and validations

--- api/controllers/shipments.controller.js
+++ api/controllers/shipments.controller.js
@@ -4,7 +4,9 @@ class ShipmentsController {
   static async getShipments(req, res, next) {
     try {
       const allShipments = await ShipmentServices.getAllShipments();
-
+      if (!allShipments) {
+        return res.status(401).send("Shipment not found!");
+      }
       res.status(200).json(allShipments);
     } catch (error) {
       next(error);
@@ -12,9 +14,12 @@ class ShipmentsController {
   }
   static async getOneShipment(req, res, next) {
     try {
-      const allShipments = await ShipmentServices.getOneShipment(req.params.id);
+      const oneShipment = await ShipmentServices.getOneShipment(req.params.id);
 
-      res.status(200).json(allShipments);
+      if (!oneShipment) {
+        return res.status(401).send("Shipment not found!");
+      }
+      res.status(200).json(oneShipment);
     } catch (error) {
       next(error);
     }
@@ -23,7 +28,7 @@ class ShipmentsController {
     try {
       const newShipment = await ShipmentServices.createShipment(req.body);
 
-      res.status(200).json({ newShipment });
+      res.status(201).json({ newShipment });
     } catch (error) {
       next(error);
     }
