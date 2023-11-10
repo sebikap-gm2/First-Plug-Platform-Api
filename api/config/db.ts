828d820d2dee3fb8399cb6a11c0164a41c08d37c
Tute22 <agustinsandoval1601@gmail.com>
2023-11-10 11:07:30 -0300

    fix: fixed import and errors in db.ts

--- api/config/db.ts
+++ api/config/db.ts
@@ -1,12 +1,9 @@
 require("dotenv").config();
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 export const connectToDatabase = async () => {
   try {
-    return await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    return await mongoose.connect(process.env.MONGO_URI!);
   } catch (error) {
     console.error("Failed to connect to database", error);
   }
