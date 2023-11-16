98c54a7d82812d12222a09b2ab5cbc0360d52dd3
Esteban Rodriguez <nahuelr.developer@gmail.com>
2023-11-16 18:43:20 -0300

    fix: optional validation added in addition to correcting the state

--- api/types/product.ts
+++ api/types/product.ts
@@ -1,14 +1,27 @@
 export type Product = {
   _id: string;
-  category: string;
-  model: string;
-  color: string;
-  screen: string;
-  keyboard: string;
-  processor: string;
-  ram: string;
-  status: ["Available", "Delivered"];
-  imgUrl: string;
+  name?: string;
+  description?: string;
+  category?: string;
+  model?: string;
+  color?: string;
+  screen?: string;
+  keyboard?: string;
+  processor?: string;
+  ram?: string;
+  storage?: string;
+  gpu?: string;
+  serialNumber?: string;
+  price?: string;
+  status?: ProductStatus;
+  imgUrl?: string;
   stock: number;
   __v: number;
 };
+
+export const PRODUCT_STATUSES = {
+  available: "Available",
+  delivered: "Delivered",
+} as const;
+
+export type ProductStatus = keyof typeof PRODUCT_STATUSES;
