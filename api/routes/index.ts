1a38d936eb09a6cecb28db6d42fa798de0ab20bd
Esteban Rodriguez <nahuelr.developer@gmail.com>
2023-11-15 21:59:58 -0300

    chore: removing spaces

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -2,7 +2,6 @@ import express from "express";
 export const router = express.Router();
 
 import { authRoutes } from "./auth.routes";
-
 import { teamMembersRoutes } from "../routes/teamMembers.routes";
 import { teamsRoutes } from "./teams.routes";
 import { ordersRoutes } from "./orders.routes";
@@ -13,7 +12,5 @@ router.use("/auth", authRoutes);
 router.use("/teamMembers", teamMembersRoutes);
 router.use("/teams", teamsRoutes);
 router.use("/orders", ordersRoutes);
-
 router.use("/shipments", shipmentRoutes);
-
 router.use("/products", productRoutes);
