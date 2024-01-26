bc7f9a5da87f555a9bc9b7c414c065973ced7a22
Esteban Nahuel Rodriguez <nahuelr.developer@gmail.com>
2024-01-26 15:16:52 -0300

    fix: new middlewares for the use of tokens

---
+++ api/middleware/jwt.middleware.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express";
+import { env } from "../config";
+import jwt from "jsonwebtoken";
+
+export class JwtMiddleware {
+  static async canActivate(req: Request, res: Response, next: NextFunction) {
+    const token = JwtMiddleware.extractTokenFromHeaders(req);
+
+    if (!token) {
+      res.status(401).json({ message: "Missing authentication token" });
+      return;
+    }
+
+    try {
+      const payload = jwt.verify(token, env.JWTSECRETKEY);
+      req["user"] = payload;
+    } catch (error) {
+      res.status(401).json({ message: "Missing authentication token" });
+      return;
+    }
+
+    next();
+  }
+
+  static extractTokenFromHeaders(request: Request) {
+    const [type, token] = request.headers.authorization?.split(" ") ?? [];
+    return type === "Bearer" ? token : undefined;
+  }
+}
