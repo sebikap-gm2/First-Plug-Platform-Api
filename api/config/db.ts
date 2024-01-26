ba0224e6f994f67c54a6686a311dcfc6ec36c68b
Esteban Nahuel Rodriguez <nahuelr.developer@gmail.com>
2024-01-26 15:11:51 -0300

    chore: add todo

--- api/config/db.ts
+++ api/config/db.ts
@@ -5,6 +5,7 @@ import mongoose, { ConnectOptions } from "mongoose";
 dotenv.config();
 mongoose.set("strictQuery", true);
 
+// TODO: find a way that if it doesn't find the db we handle the error
 export const connectToDatabase = async (dbName: ConnectOptions["dbName"]) => {
   try {
     return await mongoose.connect(env.MONGO_URI, { dbName });
