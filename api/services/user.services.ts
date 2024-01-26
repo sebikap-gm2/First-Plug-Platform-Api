0e499e05e1a5ad435d81e23bbd7b71f27708a303
Esteban Nahuel Rodriguez <nahuelr.developer@gmail.com>
2024-01-26 15:21:02 -0300

    feat: user service

--- api/services/user.services.ts
+++ api/services/user.services.ts
@@ -1,8 +1,23 @@
-import { User } from "api/types";
+import { User, CreationUser } from "../types";
 import { UserRepository } from "../models";
+import { Types } from "mongoose";
 
 export class UserService {
-  static async getByEmail(email: User['email']) {
+  static async getById(id: string) {
+    return await UserRepository.findOne({
+      _id: new Types.ObjectId(id),
+    }).exec();
+  }
+
+  static async getByEmail(email: User["email"]) {
     return await UserRepository.findOne({ email }).exec();
+  }
+
+  static async create(dto: CreationUser) {
+    const user = await this.getByEmail(dto.email);
+
+    if (user) throw new Error("email duplicated");
+
+    await UserRepository.create(dto);
   }
 }
