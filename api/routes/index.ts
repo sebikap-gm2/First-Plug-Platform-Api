0543332c257fe944bcb14eb3ab68c2807625a7f9
Esteban Nahuel Rodriguez <nahuelr.developer@gmail.com>
2024-01-26 15:19:15 -0300

    feat: token middleware activated

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -7,10 +7,11 @@ import { ordersRoutes } from "./orders.routes";
 import { productRoutes } from "./product.routes";
 import { shipmentRoutes } from "./shipments.routes";
 import { teamsRoutes } from "./teams.routes";
+import { JwtMiddleware } from "../middleware";
 
 router.use("/auth", authRoutes);
-router.use("/members", membersRoutes);
-router.use("/orders", ordersRoutes);
-router.use("/shipments", shipmentRoutes);
-router.use("/products", productRoutes);
-router.use("/teams", teamsRoutes);
+router.use("/members", JwtMiddleware.canActivate, membersRoutes);
+router.use("/orders", JwtMiddleware.canActivate, ordersRoutes);
+router.use("/shipments", JwtMiddleware.canActivate, shipmentRoutes);
+router.use("/products", JwtMiddleware.canActivate, productRoutes);
+router.use("/teams", JwtMiddleware.canActivate, teamsRoutes);
