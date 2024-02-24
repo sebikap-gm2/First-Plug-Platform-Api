6267eb6f401d260955e6715f22671bb9a42dff8c
Esteban Nahuel Rodriguez <nahuelr.developer@gmail.com>
2024-02-24 13:51:27 -0300

    feat: validations for members load

--- api/validations/index.ts
+++ api/validations/index.ts
@@ -1,1 +1,2 @@
 export * from "./product.validation";
+export * from "./member.validation";
---
+++ api/validations/member.validation.ts
@@ -0,0 +1,26 @@
+import { isISODate } from "../utils";
+import { z } from "zod";
+
+export const memberSchemaValidationStore = z.object({
+  firstName: z.string().min(1),
+  lastName: z.string().min(1),
+  dateOfBirth: z
+    .string()
+    .refine(isISODate, { message: "Not a valid ISO string date " }),
+  phone: z.string().min(1),
+  email: z.string().min(1).email(),
+  DNI: z.string().min(1),
+  jobPosition: z.string().min(1),
+  city: z.string().min(1),
+  zipCode: z.string().min(1),
+  address: z.string().min(1),
+  appartment: z.string().nullable(),
+  joiningDate: z
+    .string()
+    .refine(isISODate, { message: "Not a valid ISO string date " }),
+  timeSlotForDelivery: z.string(),
+  additionalInfo: z.string().nullable(),
+  teams: z.array(z.string()),
+});
+
+export const memberCollection = z.array(memberSchemaValidationStore);
