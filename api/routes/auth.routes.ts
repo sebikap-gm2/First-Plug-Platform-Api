d7457ad9e388caa45df29998c44fa2a5dc0803e0
Esteban Nahuel Rodriguez <nahuelr.developer@gmail.com>
2024-03-17 18:19:44 -0300

    feat: change password

--- api/routes/auth.routes.ts
+++ api/routes/auth.routes.ts
@@ -2,7 +2,7 @@ import express from "express";
 export const authRoutes = express.Router();
 
 import { AuthController } from "../controllers";
-import { RefreshMiddleware } from "../middleware";
+import { JwtMiddleware, RefreshMiddleware } from "../middleware";
 
 authRoutes.post("/register", AuthController.register);
 authRoutes.post("/login", AuthController.login);
@@ -10,4 +10,9 @@ authRoutes.post(
   "/refresh",
   RefreshMiddleware.canActivate,
   AuthController.refresh
+);
+authRoutes.post(
+  "/change-password",
+  JwtMiddleware.canActivate,
+  AuthController.changePassword
 );
